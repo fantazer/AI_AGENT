2cd4ad2306a1e4b1bb7ccc5198957fbe92398a74
FANTAZER <kuznecovs45@gmail.com>
2018-10-23 14:41:14 +0300

    Initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,294 @@
+$(document).ready(function () {
+	var wow = new WOW(
+		{
+			boxClass: 'wow',
+			animateClass: 'animated',
+			offset: 300,
+			mobile: true,
+			live: true,
+		}
+	);
+	wow.init();
+	//video popup
+	$('.fancybox').fancybox();
+	//video popup === end
+
+	//slider init
+	$('.ability-slider').slick({
+		slidesToShow: 1,
+		autoplay: false,
+		arrows: false
+	});
+	$('.ability-slider').on("afterChange", function () {
+		var currentActive = $('.ability-slider').slick('slickCurrentSlide');
+		console.log(currentActive);
+		$('.ability-list__el').removeClass('ability-list__el--active');
+		$('.ability-list__el').eq(currentActive).addClass('ability-list__el--active');
+	});
+	$('.ability-list__el').click(function(){
+		$('.ability-list__el').removeClass('ability-list__el--active');
+		$(this).addClass('ability-list__el--active');
+		var currentActive = $(this).index();
+		$('.ability-slider').slick('slickGoTo', currentActive - 1);
+	});
+	$('.slider-control--right').click(function(){
+		$(this).closest(".slider-wrap").find(".slider-item").slick('slickNext');
+	});
+
+	$('.slider-control--left').click(function(){
+		$(this).closest(".slider-wrap").find(".slider-item").slick('slickPrev');
+	});
+	//slider init end
+
+	//video bg
+	$('.header-wrap').vide(
+		{
+			webm: 'img/work.webm' //путь к файлу
+		},
+		{
+			muted: true,
+			loop: true,
+			posterType: "webm" // нужный тип файлов
+		});
+	//video bg
+
+	//modal
+	var modalState = {
+		"isModalShow": false, //state show modal
+		"scrollPos": 0
+	};
+	$('.modal-content').click(function (event) {
+		event.stopPropagation();
+	});
+
+	var openModal = function () {
+		if (!$('.modal-layer').hasClass('modal-layer-show')) {
+			$('.modal-layer').addClass('modal-layer-show');
+			modalState.scrollPos = $(window).scrollTop();
+			$('body').css({
+				overflow: 'hidden',
+				position: 'fixed',
+				overflowY: 'scroll',
+				top: -modalState.scrollPos,
+				width: '100%'
+			});
+		}
+		modalState.isModalShow = true;
+	};
+
+	var closeModal = function () {
+		$('.modal-layer').removeClass('modal-layer-show');
+		$('body').css({
+			overflow: '',
+			position: '',
+			top: modalState.scrollPos
+		});
+		$(window).scrollTop(modalState.scrollPos);
+		$('.modal').removeClass('modal__show');
+		modalState.isModalShow = false;
+	};
+
+	var initModal = function (el) {
+		openModal();
+		$('.modal').each(function () {
+			if ($(this).data('modal') === el) {
+				$(this).addClass('modal__show')
+			} else {
+				$(this).removeClass('modal__show')
+			}
+		});
+		var modalHeightCont = $(window).height();
+		$('.modal-filter').height(modalHeightCont);
+
+	};
+
+	$('.modal-get').click(function () {
+		var currentModal = $(this).data("modal");
+		initModal(currentModal);
+	});
+
+	$('.modal-layer , .modal-close').click(function () {
+		closeModal();
+	});
+
+	//modals===end
+
+	//validate
+	$.validator.addMethod('fnType', function(value, element) {
+		return value.match(/^[+-]?\d+$/);
+	},'Введите правильный телефон');
+	$('.validate-form').each(function () {
+		var curentForm = $(this);
+		$(this).validate({
+			highlight: function (element) { //даем родителю класс если есть ошибка
+				$(element).parent().addClass("field-error");
+			},
+			unhighlight: function (element) {
+				$(element).parent().removeClass("field-error");
+			},
+			rules: { //правила для полей
+				name: {
+					required: true,
+				},
+				phone: {
+					required: true,
+					minlength: 5,
+					fnType: true
+				},
+				comment: {
+					required: true,
+					minlength: 5,
+				},
+				agree: {
+					required: true
+				}
+			},
+			messages: {
+				name: {
+					required: 'Обязательное поле',
+				},
+				phone: {
+					required: 'Обязательное поле',
+					number: 'Введите правильный номер',
+					minlength: 'Номер должен быть длиннее',
+				},
+				comment: {
+					required: 'Обязательное поле',
+					minlength: 'Сообщение должно быть длиннее',
+				},
+				agree: {
+					required: false,
+				}
+			},
+			submitHandler: function (form) {
+				$.ajax({ //отправка ajax
+					type: "POST",
+					url: "sender.php",
+					data: $(form).serialize(),
+					timeout: 3000,
+				});
+				closeModal();
+				initModal("truemessage");
+			/*	setTimeout(function () {
+					closeModal();
+					$(':input', '.validate-form') //очитска формы от данных
+						.not(':button, :submit, :reset, :hidden')
+						.val('')
+						.removeAttr('checked')
+						.removeAttr('selected')
+				}, 2500)
+*/
+			}
+		});
+	});
+	function detectIE() {
+		var ua = window.navigator.userAgent;
+
+		var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+			// IE 10 or older => return version number
+			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+			// IE 11 => return version number
+			var rv = ua.indexOf('rv:');
+			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+
+		var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+			// Edge (IE 12+) => return version number
+			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		}
+
+		// other browser
+		return false;
+	}
+
+	if (detectIE() <= 14 && detectIE()) {
+		$('body').empty();
+		$('body').prepend('' +
+			'<div class="old-browser">' +
+			'<div class="old-browser-text"> Сайт не поддерживае Браузер Internet Explorer</div><br>' +
+			'<div class="old-browser-text"> Установите <br><br> Chrome Firefox Opera </div><br>' +
+			'</div>');
+	}
+	//for init SVG
+
+	// ==== clear storage =====
+	localStorage.clear();
+	sessionStorage.clear();
+	$(window).unload(function () {
+		localStorage.clear();
+	});
+	// ==== clear storage end =====
+
+
+	/* ###### For SlideToggle Elements  ######*/
+	/*var hideToggle = function(targetClick,toggleEl) {
+		$(targetClick).click(function(event){
+				event.stopPropagation();
+				$(toggleEl).slideToggle("fast");
+		});
+		$(toggleEl).on("click", function (event) {
+			event.stopPropagation();
+		});
+		$(document).on("click", function () {
+				$(toggleEl).hide();
+		});
+	}
+	hideToggle('.icon-bars','.top-menu_link');*/
+
+})
+
+//cash SVG
+
+;(function (window, document) {
+	'use strict';
+
+	var file = 'img/pack.html',
+		revision = 1;
+
+	if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect)
+		return true;
+
+	var isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
+		request,
+		data,
+		insertIT = function () {
+			document.body.insertAdjacentHTML('afterbegin', data);
+		},
+		insert = function () {
+			if (document.body) insertIT();
+			else document.addEventListener('DOMContentLoaded', insertIT);
+		};
+
+	if (isLocalStorage && localStorage.getItem('inlineSVGrev') == revision) {
+		data = localStorage.getItem('inlineSVGdata');
+		if (data) {
+			insert();
+			return true;
+		}
+	}
+
+	try {
+		request = new XMLHttpRequest();
+		request.open('GET', file, true);
+		request.onload = function () {
+			if (request.status >= 200 && request.status < 400) {
+				data = request.responseText;
+				insert();
+				if (isLocalStorage) {
+					localStorage.setItem('inlineSVGdata', data);
+					localStorage.setItem('inlineSVGrev', revision);
+				}
+			}
+		}
+		request.send();
+	}
+	catch (e) {
+	}
+
+}(window, document));
